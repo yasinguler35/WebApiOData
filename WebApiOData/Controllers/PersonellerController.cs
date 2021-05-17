7f85683ff9e26af70f0b48cd775afa75d50e7fc7
yasinguler007 <yasinguler58@outlook.com>
2021-05-17 12:17:22 +0300

    personeller listesi methodu yazıldı GetPersoneller

--- WebApiOData/Controllers/PersonellerController.cs
+++ WebApiOData/Controllers/PersonellerController.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiOData.Models;
 
 namespace WebApiOData.Controllers
 {
     public class PersonellerController : ApiController
     {
+        private ODataServis db = new ODataServis();
+        public IQueryable<Personeller> GetPersoneller()
+        {
+            return db.personeller;
+        }
     }
 }
