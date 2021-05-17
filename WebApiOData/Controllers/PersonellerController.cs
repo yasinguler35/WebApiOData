46b7ece1a16fa0670a846b38470c2cb3633e3d7a
yasinguler007 <yasinguler58@outlook.com>
2021-05-17 13:28:05 +0300

    tabloda select yapma

--- WebApiOData/Controllers/PersonellerController.cs
+++ WebApiOData/Controllers/PersonellerController.cs
@@ -11,6 +11,8 @@ namespace WebApiOData.Controllers
 {
     public class PersonellerController : ODataController
     {
+        //tabloda select yapma
+        //http://localhost:57962/odata/Personeller?$select=perId,perAd
         private ODataServis db = new ODataServis();
         [EnableQuery]
         public IQueryable<Personeller> GetPersoneller()
