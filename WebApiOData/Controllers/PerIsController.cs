439a6fefa0c42fbcac1f7164be8e0aaa7474c2ce
yasinguler007 <yasinguler58@outlook.com>
2021-05-17 13:22:31 +0300

    peris controlleri yazıldı odata kullanılararak

---
+++ WebApiOData/Controllers/PerIsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+using WebApiOData.Models;
+
+namespace WebApiOData.Controllers
+{
+    public class PerIsController : ODataController
+    {
+        private ODataServis db = new ODataServis();
+        [EnableQuery]
+        public IQueryable<PerIs> GetPerIs()
+        {
+            return db.perIs;
+        }
+    }
+}
