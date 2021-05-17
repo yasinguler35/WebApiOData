d249fe2582a782c25d8c68d3bf1e1a1dd321be03
yasinguler007 <yasinguler58@outlook.com>
2021-05-17 12:35:13 +0300

    sorgu yapılabilmesi için  [EnableQuery] eklendi

--- WebApiOData/Controllers/PersonellerController.cs
+++ WebApiOData/Controllers/PersonellerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.OData;
 using WebApiOData.Models;
 
 namespace WebApiOData.Controllers
@@ -11,6 +12,7 @@ namespace WebApiOData.Controllers
     public class PersonellerController : ApiController
     {
         private ODataServis db = new ODataServis();
+        [EnableQuery]
         public IQueryable<Personeller> GetPersoneller()
         {
             return db.personeller;
